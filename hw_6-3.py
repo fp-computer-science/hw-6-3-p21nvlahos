fed850ad4f6f46a3c0cbd67b526fed511738ae39
Niko <p21nvlahos@fairfieldprep.org>
2020-10-29 21:43:28 -0400

    Add hw6-3

---
+++ hw_6-3.py
@@ -0,0 +1,32 @@
+# Author: NV 10/29/2020
+import math
+
+# Question 1
+print(math.sqrt(529))
+
+# Question 2
+print(math.sin(0))
+
+# Question 3
+print(math.cos(2 * math.pi))
+
+# Question 4
+print(math.pow(2.5, 5))
+
+# Question 5
+print(math.log(math.e))
+
+# Question 6
+print(math.pow(math.e, 4))
+
+# Question 7
+print(math.ceil(-12.9))
+
+# Question 8
+print(math.floor(7.25))
+
+# Question 9
+print((math.log(1000)) * 2)
+
+# Question 10
+print(math.radians(180))
